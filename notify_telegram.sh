1cdf8acae1add98fe3b116a50afa6e65a0590485
greksw <greksw@gmail.com>
2024-09-04 11:29:31 +0300

    Update notify_telegram.sh
    # Имя хоста (название сервера)
    HOSTNAME=$(hostname)

--- notify_telegram.sh
+++ notify_telegram.sh
@@ -5,8 +5,11 @@ TOKEN="your_telegram_bot_token"
 # ID чата (ваш User ID)
 CHAT_ID="your_chat_id"
 
+# Имя хоста (название сервера)
+HOSTNAME=$(hostname)
+
 # Сообщение
-MESSAGE="SSH login detected: $(whoami) from $(hostname -I | awk '{print $1}')"
+MESSAGE="SSH login detected on server: ${HOSTNAME}. User: $(whoami) from IP: $(hostname -I | awk '{print $1}')"
 
 # Отправка сообщения
 curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \
